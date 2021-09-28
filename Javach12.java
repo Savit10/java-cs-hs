ad93c6b889c8f249ebcd28d14382db9c5db32618
Savit10 <savita@my.ofs.edu.sg>
2021-09-28 12:29:58 +0800

    Update Java

--- Javach12.java
+++ Javach12.java
@@ -3,12 +3,12 @@ public class Javach12
 	public static void main(String args[])
 	{
 		
-			int a = IBIO.inputInt("Enter a starting number: ");
-			int d = IBIO.inputInt("Enter the increment: ");
-			int step = IBIO.inputInt("Enter a step: ");
-			for ( int i=0; i<=step; i++)
+			int a = 3; /*IBIO.inputInt("Enter a starting number: ")*/
+			int d = 2;  /*IBIO.inputInt("Enter the increment: ")*/
+			int step = 4; /*IBIO.inputInt("Enter a step: ")*/
+			for (int i=0; i<step; i++)
 			{
-				IBIO.output(a);
+				System.out.println(a); /*IBIO.output(a)*/
 				a = a+d;
 			}		
 	}
