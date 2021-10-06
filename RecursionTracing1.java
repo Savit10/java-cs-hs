e5032b583095b83563e3fb5c536720744d663771
Savit10 <savita@my.ofs.edu.sg>
2021-10-06 09:23:49 +0800

    t

--- RecursionTracing1.java
+++ RecursionTracing1.java
@@ -2,8 +2,6 @@ public class RecursionTracing1
 {
 	public static int sum(int n)
 	{
-		System.out.println("N is " + n);
-		System.out.println("Sum = " + (n + sum(n-1)));
 		if (n==1)
 		{
 			return 1;
@@ -16,7 +14,6 @@ public class RecursionTracing1
 	}	
 	public static int result(int n)
 	{
-		System.out.println("N is " + n);
 		if(n==1)
 		{
 			return 2;
@@ -26,11 +23,42 @@ public class RecursionTracing1
 			return 2 * result(n-1);
 		}
 	}
+	
+	public static int mystery(int n, int a, int d)
+	{
+		if (n==1)
+			return a;
+		else
+			return d + mystery (n-1, a, d);
+	}
+	
+	public static int f(int k, int n)
+	{
+		if (n==k)
+		{
+			return k;
+		}
+		else
+		{
+			if (n>k)
+			{
+				return f(k,n-k);
+			}
+			else
+			{
+				return f(k-n,n);
+			}
+		}
+	}
 	public static void main (String args[])
 	{
 		System.out.println(sum(5));
 		System.out.println("========");
 		System.out.println(result(5));
+		System.out.println("========");
+		System.out.println(mystery(3,2,6));
+		System.out.println("========");
+		System.out.println(f(6,8));
 	}
 }
 	
