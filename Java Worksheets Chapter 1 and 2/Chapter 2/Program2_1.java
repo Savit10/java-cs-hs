0f2946d5c30e8f7f932344d9c3cbbdb853177f43
Savit10 <savita@my.ofs.edu.sg>
2021-10-06 18:23:02 +0800

    t

---
+++ Java Worksheets Chapter 1 and 2/Chapter 2/Program2_1.java
@@ -0,0 +1,11 @@
+public class Program2_1 {
+	
+	public static void main (String[] args) {
+		int n = IBIO.inputInt("input a nymber between 50 and 60: ");
+		if(n<50 || n>60)
+		{
+			IBIO.output("the number was too big");
+		}
+	}
+}
+
---
+++ Java Worksheets Chapter 1 and 2/Mock Test/maxof3.java
@@ -0,0 +1,23 @@
+public class maxof3 {
+	
+	public static void main (String[] args) {
+		int x = IBIO.inputInt("No. 1: ");
+		int y = IBIO.inputInt("No. 2: ");
+		int z = IBIO.inputInt("No. 3: ");
+		int max = 0;
+		if (x>max)
+		{
+			max=x;
+		}
+		if (y>max)
+		{
+			max=y;
+		}
+		if (z>max)
+		{
+			max=z;
+		}
+		IBIO.output(max);
+	}
+}
+
