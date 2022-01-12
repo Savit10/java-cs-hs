bde72d7bab57de9906fd663f899b48084b16e2cb
Savit10 <savita@my.ofs.edu.sg>
2022-01-12 20:12:13 +0800

    u

---
+++ G11 Semester 2/Java Worksheets/Chapter 6/Chapter6.java
@@ -0,0 +1,18 @@
+public class Chapter6 
+{	
+	public static void main (String[] args) 
+	{
+		int num = IBIO.inputInt("enter number of lines ");
+		String aa = stars(num);
+		for (int i = 0; i < num; i++)
+			IBIO.output(aa);
+	}
+	static String stars(int n)
+	{ 
+	  String xx = "";
+	  for (int i = 0; i < n; i++)
+		  xx = xx + "*";
+	  return xx;
+	} 
+}
+
