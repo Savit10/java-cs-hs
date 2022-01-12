ea29c190dd0fe43eefd6b764718490427c5d56e4
Savit10 <savita@my.ofs.edu.sg>
2022-01-12 20:01:19 +0800

    y

--- Quarter 1/Chapter 1/Program1_4.java
+++ Quarter 1/Chapter 1/Program1_4.java
@@ -5,7 +5,8 @@ public class Program1_4
 	public static void main (String args[])
 	{
 		int term = 1;
-		for(int i=1; i<=100; i++)
+		
+		for(int i = 1; i<=100; i++)
 		{
 			System.out.println(term);
 			term = term + i + 1;
